cf255d1c339bee5b3c1aad0fbf242d45c5870263
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-09-17 21:09:34 -0300

    Projeto Criado, DockerFile, Docker-compose e application.properties configurado

---
+++ src/main/java/devs/java10x/FinanSync/FinanSync.java
@@ -0,0 +1,13 @@
+package devs.java10x.FinanSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanSync {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanSync.class, args);
+	}
+
+}
---
+++ src/test/java/devs/java10x/FinanSync/FinanSyncTests.java
@@ -0,0 +1,13 @@
+package devs.java10x.FinanSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanSyncTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
